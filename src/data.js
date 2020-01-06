21930e6d75e92aa98c91be97c6b9171a6efbbc56
Leigh Rogers <limarkey@gmail.com>
2020-01-06 17:51:07 -0600

    added photo and updated work content

--- src/data.js
+++ src/data.js
@@ -1,12 +1,14 @@
+import aboutImage from "./components/images/leigh-fun.jpg"
+
 export default
     {
         //(Please Do Not Remove The comma(,) after every variable)
         //Change The Website Template
         name :'leigh rogers.',
         headerTagline: [//Line 1 For Header
-                        'Hello! I am Leigh Rogers.',
+                        'Hello! I\'m Leigh,',
                         //Line 2 For Header
-                        'UX/UI | Front-end developer | Digital strategist.',
+                        'a UI/UX app developer.',
                         //Line 3 For Header
                         'Check out my work.'
     ],
@@ -14,45 +16,45 @@ export default
         contactEmail:'limarkey@gmail.com',
         // Add Your About Text Here
         abouttext: "With over 10 years of experience in digital strategy, website management, and now front-end development, I have focused on producing high-quality digital products. A big picture thinker and strategist at heart, I love working on projects from conception to final execution, with the skills to implement a web project in its entirety. My extensive experience with digital strategy and management means I understand external marketing priorities as well as digital end-user needs. Attending Nashville Software School has allowed me to apply software development to the planning and implementation process of building websites that I’ve known for over a decade.",
-        aboutImage:'https://images.unsplash.com/photo-1521587765099-8835e7201186?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+        aboutImage: aboutImage,
        //Change This To Hide The Image of About Section (True Or False)
        ShowAboutImage:true,//true or false (Change Here)
        // Change Projects Here
        projects:[
            {
-            id: 1,//DO NOT CHANGE THIS (Please)😅
-            title:'Project One', //Project Title - Add Your Project Title Here
-             service:'UI/UX Design', // Add Your Service Type Here
-            //Project Image - Add Your Project Image Here
-             imageSrc:"https://images.unsplash.com/photo-1558104631-0fa41a8f6c20?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-             //Project URL - Add Your Project Url Here
-             url:'http://chetanverma.com/'
+            id: 1,
+                title:'Pac-Man Ghost Social Network', //Project Title - Add Your Project Title Here
+                service:'React App Development', // Add Your Service Type Here
+                //Project Image - Add Your Project Image Here
+                imageSrc:"https://images.unsplash.com/photo-1558104631-0fa41a8f6c20?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+                //Project URL - Add Your Project Url Here
+                url:'http://chetanverma.com/'
             },
             {
-                id: 2,//DO NOT CHANGE THIS (Please)😅
-                title: 'Project Two',
-                service: 'Website Development',
+                id: 2,
+                title: 'Safe Trip',
+                service: 'React App Development',
                 imageSrc: "https://images.unsplash.com/photo-1554866585-cd94860890b7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
                 url: 'http://chetanverma.com/'
             },
             {
-                id: 3,//DO NOT CHANGE THIS (Please)😅
-                title: 'Project Three',
-                service: 'Web App',
+                id: 3,
+                title: 'NSS Cohort 35 Website',
+                service: 'UI/UX Website Development',
                 imageSrc: "https://images.unsplash.com/photo-1511500118080-275313ec90a1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
                 url: 'http://chetanverma.com/'
             },
             {
-                id: 4,//DO NOT CHANGE THIS (Please)😅
-                title: 'Project Four',
-                service: 'Branding',
+                id: 4,
+                title: 'Bangazon',
+                service: 'UI/UX Design',
                 imageSrc: "https://images.unsplash.com/photo-1558452919-08ae4aea8e29?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
                 url: 'http://chetanverma.com/'
            }
 
                     /*
                     If You Want To Add More Project just Copy and Paste This At The End (Update the id Respectively)
-                ,{
+            {
                 id: 5,
                 title: 'Project Five',
                 service: 'Something Amazing',
