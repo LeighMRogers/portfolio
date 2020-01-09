9042968a94b1dc4c0c25077536b7aa56ff53cb40
Leigh Rogers <limarkey@gmail.com>
2020-01-09 15:52:30 -0600

    Updates

---
+++ precache-manifest.a9e5bbf2a6c4634a89781970dad868d2.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e249b1d05b13aad3f3f2a51a16566b5f",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "3bbdc9678fe2c86ddb1b",
+    "url": "/portfolio/static/css/main.6abec7d3.chunk.css"
+  },
+  {
+    "revision": "806b88bd4ee40f873f70",
+    "url": "/portfolio/static/js/2.9690ac1e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/portfolio/static/js/2.9690ac1e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3bbdc9678fe2c86ddb1b",
+    "url": "/portfolio/static/js/main.95db1dd5.chunk.js"
+  },
+  {
+    "revision": "f5ff1fe24da837620bb5",
+    "url": "/portfolio/static/js/runtime-main.c507fcf5.js"
+  },
+  {
+    "revision": "f91d163ba5f41b32818b1d6b8e9acda5",
+    "url": "/portfolio/static/media/C35Website.f91d163b.png"
+  },
+  {
+    "revision": "4b28b7232362a2f5c85e16f136c474b1",
+    "url": "/portfolio/static/media/GhostPac.4b28b723.png"
+  },
+  {
+    "revision": "29c74fe9933f1cfd3d25631289d3661e",
+    "url": "/portfolio/static/media/HeadshotFun.29c74fe9.jpg"
+  },
+  {
+    "revision": "56bf20b89f0fe7229bddd63821150242",
+    "url": "/portfolio/static/media/SafeTripMyItineraries.56bf20b8.png"
+  },
+  {
+    "revision": "c329ed2575f969458ac6a85efdf75952",
+    "url": "/portfolio/static/media/bangazon-figma-mockup.c329ed25.jpg"
+  }
+]);
